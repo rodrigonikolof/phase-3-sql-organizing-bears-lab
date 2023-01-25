fc1b7ccd4a9b08358fc2edcec1ae14bae9ae2506
Rodrigo Nikolof <rodrigo.nikolof@outlook.com>
2023-01-26 10:13:04 +1100

    commit tree 3b26d27337e29d87d75cd9693428144aa3d4cc95

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, sex) VALUES ('Mr. Chocolate', 'M');
+INSERT INTO bears (name, sex) VALUES ('Rowdy', 'F');
+INSERT INTO bears (name, sex) VALUES ('Tabitha', 'F');
+INSERT INTO bears (name, sex) VALUES ('Sergeant Brown', 'F');
+INSERT INTO bears (name, sex) VALUES ('Melissa', 'M');
+INSERT INTO bears (name, sex) VALUES ('Grinch', 'M');
+INSERT INTO bears (name, sex) VALUES ('Wendy', 'M');
+INSERT INTO bears (name, sex) VALUES (NULL, 'F');
